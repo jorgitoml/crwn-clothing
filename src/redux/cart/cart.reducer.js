4b60b4b3b810bc672bd8d78560acbbdff4c115d0
Jordi Mestres López <jordi.mestres.l@gmail.com>
2019-08-26 12:18:38 +0200

    .

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import { CartActionTypes } from './cart.types';
-import { addItemToCart } from './cart.utils';
+import { addItemToCart, removeItemFromCart } from './cart.utils';
 
 const INITIAL_STATE = {
 	hidden: true,
@@ -18,6 +18,19 @@ const cartReducer = (state = INITIAL_STATE, { type, payload }) => {
 				...state,
 				cartItems: addItemToCart(state.cartItems, payload)
 			};
+		case CartActionTypes.REMOVE_ITEM:
+			return {
+				...state,
+				cartItems: removeItemFromCart(state.cartItems, payload)
+			};
+		case CartActionTypes.CLEAR_ITEM_FROM_CART:
+			return {
+				...state,
+				cartItems: state.cartItems.filter(
+					cartItem => cartItem.id !== payload.id
+				)
+			};
+
 		default:
 			return state;
 	}
