4017663a0e2570e24fe7e8c53794b5f007aa40e9
Jordi Mestres López <jordi.mestres.l@gmail.com>
2019-08-28 12:04:00 +0200

    .

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
-import { composeWithDevTools } from 'redux-devtools-extension/logOnly';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 
 import rootReducer from './root-reducer';
 
