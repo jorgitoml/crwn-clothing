fca8e360f754a11054cda5de3d4a449df8f3acaf
Jordi Mestres López <jordi.mestres.l@gmail.com>
2019-08-28 11:57:28 +0200

    .

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension/logOnly';
 
 import rootReducer from './root-reducer';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = composeWithDevTools({});
 
 const middlewares = [];
 if (process.env.NODE_ENV === 'development') {
