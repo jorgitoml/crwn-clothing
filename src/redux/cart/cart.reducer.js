e3cd0e2a9c944b3f5d9f1b5554089ee9f7407e6c
Jordi Mestres López <jordi.mestres.l@gmail.com>
2019-10-04 17:22:04 +0200

    Cart Sagas

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -31,6 +31,12 @@ const cartReducer = (state = INITIAL_STATE, { type, payload }) => {
 				)
 			};
 
+		case CartActionTypes.CLEAR_CART:
+			return {
+				...state,
+				cartItems: []
+			};
+
 		default:
 			return state;
 	}
